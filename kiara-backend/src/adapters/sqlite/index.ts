e78e3b986e648f90c429c7ef3abc6d2b2465d8c5
syntaxbullet <syntaxbullet@protonmail.com>
2023-07-19 15:14:03 +0200

    fix: remove unused adapter models for conversations and messages.

--- kiara-backend/src/adapters/sqlite/index.ts
+++ kiara-backend/src/adapters/sqlite/index.ts
@@ -1,11 +1,7 @@
 import Database from "better-sqlite3";
 import { UserRepo } from "./user";
-import { MessageRepo } from "./message";
-import { ConversationRepo } from "./conversation";
 
 const db = new Database("db.sqlite");
 const UserRepository = new UserRepo(db);
-const MessageRepository = new MessageRepo(db);
-const ConversationRepository = new ConversationRepo(db);
 
-export { UserRepository, MessageRepository, ConversationRepository };
+export { UserRepository };
